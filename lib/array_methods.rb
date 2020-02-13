44204dd5211259cf9b246ce9291eee7bc04899ea
CHANMOON PARK <thechanmoon@gmail.com>
2020-02-13 19:59:57 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,42 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+    # Add your solution here
+    retVal = nil
+    array.length.times |index| {
+      if(array[index] ===  value_to_find)
+      {
+        retVal = index
+        return retVal
+      }
+    }
+    return retVal
 end
 
 def find_max_value(array)
-  # Add your solution here
+    # Add your solution here
+    retVal = array[0]
+    index = 1
+    while index < array.length 
+    {
+      if(retVal < array[index])
+      {
+        retVal = array[index]
+      }
+      index += 1
+    }
+    return retVal
 end
 
 def find_min_value(array)
-  # Add your solution here
+    # Add your solution here
+        retVal = array[0]
+    index = 1
+    while index < array.length 
+    {
+      if(retVal < array[index])
+      {
+        retVal = array[index]
+      }
+      index += 1
+    }
+    return retVal
 end
